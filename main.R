b38af9b9eef7abbf518d880a34f5e11510b71fd5
ginberg <info@gerinberg.com>
2021-06-29 10:15:36 +0000

    fix Basal data view

--- main.R
+++ main.R
@@ -7,12 +7,12 @@ data   <- ctx %>% select(.ri, .ci, .y)
 matrix <- ctx$as.matrix()
 
 row_means <- rowMeans(matrix)
-row_rank  <- rank(matrix, ties.method = "min")
+row_rank  <- rank(row_means, ties.method = "min")
 row_rank  <- 1 + max(row_rank) - row_rank
 result    <- data.frame(.ri = data$.ri, .ci = data$.ci, rowMean = NaN, rowRank = NaN)
-for (i in 1:length(row_means)){
-  result$rowMean[result$.ri == i] = row_means[i]
-  result$rowRank[result$.ri == i] = row_rank[i]
+for (i in 0:length(row_means)) {
+  result$rowMean[result$.ri == i] = row_means[i+1]
+  result$rowRank[result$.ri == i] = row_rank[i+1]
 }
 
 result %>%
