acddbc6506cc02743968d963f54a81befe824079
Alexandre Maurel <alexandre.maurel@gmail.com>
2022-05-25 08:17:42 +0000

    minimize output to row only

--- main.R
+++ main.R
@@ -6,14 +6,13 @@ library(dplyr, warn.conflicts = FALSE)
 
 ctx = tercenCtx()
 
-data = ctx %>% select(.y, .ci, .ri) %>% lazy_dt();
-
-data %>%
+ctx %>% select(.y, .ri) %>%
+  lazy_dt() %>%
   group_by(.ri) %>%
   summarise(rowMean = mean(.y)) %>%
   mutate(rowRank=rank(rowMean, ties.method = "min")) %>%
-  right_join(data %>% select(-.y), by = ".ri") %>%
   as_tibble() %>%
   ctx$addNamespace() %>%
   ctx$save()
+
  
